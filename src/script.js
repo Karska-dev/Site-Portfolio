3f0d1c71a421487c580499f3ff605d6730e0cf86
Oleksandra <Alexa@tests-MacBook-Pro.local>
2020-01-23 20:26:28 -0500

    Last commit of script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
-console.warn('Temporary redirected');
-//window.location.href = "./construction/construction.html";
 
 const sections = document.querySelectorAll('section');
 //console.log(sections);
