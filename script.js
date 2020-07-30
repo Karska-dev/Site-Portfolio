712c7d151aad482901e5c420cc6724e4c8c0d764
Oleksandra <karska.dev@gmail.com>
2020-07-29 22:53:38 -0700

    Add overlay menu

---
+++ script.js
@@ -0,0 +1,9 @@
+let menuBtn = document.querySelector('.header__menu');
+let header = document.querySelector('header');
+
+menuBtn.addEventListener('click', openMenu);
+
+function openMenu() {
+
+  console.log('click-click');
+}
