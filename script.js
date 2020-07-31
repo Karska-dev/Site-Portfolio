280ebfa87e19b694bb5fe2032e23e544df1330fa
Oleksandra <karska.dev@gmail.com>
2020-07-30 22:56:52 -0700

    Add toggle button for dark/light mode switch

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function openMenu() {
 
   console.log('click-click');
 }
+
+let toggle = document.querySelector('.toggle__checkbox');
+
+toggle.addEventListener('change', switchMode);
+
+function switchMode(event) {
+  //console.log('switch-switch');
+  if (event.target.checked) {
+    header.style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+  } else {
+    header.style.backgroundColor = 'rgba(34, 34, 34, 0.5)';
+  }
+}
