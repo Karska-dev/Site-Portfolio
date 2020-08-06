42c6ce0d88d8cd457fb6fd7554942f2119c61e70
Oleksandra <karska.dev@gmail.com>
2020-08-05 23:17:22 -0700

    Add correct links. Add correct colors and designs for light css mode

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let overlay = document.querySelector('.overlay');
 let closeBtn = document.querySelector('.menu__close');
 
 let header = document.querySelector('header');
+let cssStyle = document.querySelector('#theme-style');
 
 menuBtn.addEventListener('click', openMenu);
 
 function openMenu() {
   //console.log('click-click');
   overlay.classList.add('overlay--visible');
-  
+  document.body.classList.add('noscroll');
 }
 
 closeBtn.addEventListener('click', closeMenu);
@@ -17,6 +18,7 @@ closeBtn.addEventListener('click', closeMenu);
 function closeMenu() {
   //console.log('click-click');
   overlay.classList.remove('overlay--visible');
+  document.body.classList.remove('noscroll');
 }
 
 let toggle = document.querySelector('.toggle__checkbox');
@@ -27,7 +29,9 @@ function switchMode(event) {
   //console.log('switch-switch');
   if (event.target.checked) {
     //header.classList.add('header--light');
+    cssStyle.href = "css/light.css";
   } else {
     //header.classList.remove('header--light');
+    cssStyle.href = "";
   }
 }
